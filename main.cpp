551bb768d69509c25b68ba86e9dfdd9ccb565bb8
MISHA <vol.mykhailo@gmail.com>
2023-09-23 17:46:35 +0300

    finish1

--- main.cpp
+++ main.cpp
@@ -9,27 +9,76 @@ int main() {
 	userDate.Print();
 
 	int m;
-	
 	do
 	{
 		cout << "Enter month" << endl;
 		cin >> m;
-	} 
-	while (m > 12 || m < 1);
-
+	} while (m > 12 || m < 1);
 	userDate.SetMounth(m);
 
-
-
 	int y;
-
 	do
 	{
 		cout << "Enter year" << endl;
 		cin >> y;
 	} while (y > 2024 || y < 777);
 
-	userDate.SetMounth(y);
+	userDate.SetYear(y);
 
-	
+	userDate.SetDay(userDate.EnterDay(userDate));
+
+	userDate.Print();
+
+	++userDate;
+	userDate.CompareDate(userDate);
+	userDate.Print();
+	cout << endl;
+
+
+	--userDate;
+	userDate.CompareDate(userDate);
+	userDate.Print();
+	cout << endl;
+
+
+	userDate - 10;
+	userDate.CompareDate(userDate);
+	userDate.Print();
+	cout << endl;
+
+	userDate + 15;
+	userDate.CompareDate(userDate);
+	userDate.Print();
+	cout << endl;
+
+
+	userDate -= 20;
+	userDate.CompareDate(userDate);
+	userDate.Print();
+	cout << endl;
+
+	userDate += 35;
+	userDate.CompareDate(userDate);
+	userDate.Print();
+	cout << endl;
+
+	Data userDate2(15, 3, 1950);
+	userDate2.Print();
+
+
+	if (userDate > userDate2) {
+		cout << "userDate > userDate2" << endl;
+	}
+
+	if (userDate < userDate2) {
+		cout << "userDate < userDate2" << endl;
+	}
+
+	if (userDate == userDate2) {
+		cout << "userDate == userDate2" << endl;
+	}
+
+	if (userDate != userDate2) {
+		cout << "userDate != userDate2" << endl;
+	}
 }
